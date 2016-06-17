367e2f54d40bab977cdc3d6e13595dc2d1399992
Taslim Salisu <taslim_salisu@gcitsolutions.com>
2016-06-16 20:26:46 -0400

    Bug fix
    Fixed an exception that was being thrown because the scanner object was
    closed in the method of the first game and thereby became inaccessible
    to the second game.

--- src/com/gcit/AssignmentOne.java
+++ src/com/gcit/AssignmentOne.java
@@ -5,23 +5,31 @@ import java.util.Scanner;
 public class AssignmentOne {
 
 	public static void main(String[] args) {
-		numberGuessing(50, 5);
-		pileGame();
+
+		Scanner input = new Scanner(System.in);
+		numberGuessing(50, 5, input);
+		System.out.println("The second game starts now");
+		
+		pileGame(input);
+		input.close();
 	}
 
-	private static void pileGame() {
-		System.out.println("How many chips does the initial pile contain?");
-		int numOfChips; 
-		Scanner	scanner = new Scanner(System.in);
+	private static void pileGame(Scanner scanner) {
 
-		numOfChips = scanner.nextInt();	
+
+		System.out.println("How many chips does the initial pile contain?");
+		int numOfChips;
+		//		 / scanner = new Scanner(System.in);
+
+		numOfChips = scanner.nextInt();
 		while (numOfChips % 2 == 0 || numOfChips < 3) {
 			if (numOfChips % 2 == 0) {
-				System.out.println("You have to start with an odd number of chips. Choose another number:");
+				System.out
+				.println("You have to start with an odd number of chips. Choose another number:");
 				numOfChips = scanner.nextInt();
-			}
-			else {
-				System.out.println("You have to start with at least 3 chips. Choose another number:");
+			} else {
+				System.out
+				.println("You have to start with at least 3 chips. Choose another number:");
 				numOfChips = scanner.nextInt();
 			}
 		}
@@ -35,52 +43,65 @@ public class AssignmentOne {
 		System.out.println("Enter Player two's name.");
 		playerTwo.setName(scanner.next());
 
-		while(playerOne.getName().equalsIgnoreCase(playerTwo.getName())) {
+		while (playerOne.getName().equalsIgnoreCase(playerTwo.getName())) {
 			System.out.println("Reenter player two's name.");
 			playerTwo.setName(scanner.next());
 		}
 
-		System.out.println("Player one's name is " + playerOne.getName() + " and player two's name is " + playerTwo.getName());
+		System.out.println("Player one's name is " + playerOne.getName()
+				+ " and player two's name is " + playerTwo.getName());
 		gameStart(numOfChips, playerOne, playerTwo);
-		
-		scanner.close();
-		
+
+		//		scanner.close();
+
 	}
 
 	private static void gameStart(int numOfChips, Player one, Player two) {
 		Scanner scanner = new Scanner(System.in);
 		int chipsRemoved, chipsAllowed;
-		chipsAllowed = numOfChips % 2 == 0 ? numOfChips / 2: (numOfChips - 1) / 2;
-		while(numOfChips > 0 ) {
+		chipsAllowed = numOfChips % 2 == 0 ? numOfChips / 2
+				: (numOfChips - 1) / 2;
+		while (numOfChips > 0) {
 			if (numOfChips == 1) {
-				System.out.print(one.getName() + " There is 1 chip remaining. You can only take 1");
+				System.out.print(one.getName()
+						+ " There is 1 chip remaining. You can only take 1. Please enter 1.");
 				chipsRemoved = scanner.nextInt();
 				while (chipsRemoved != 1) {
-					System.out.print("You must remove 1 chip");
+					System.out.print("You must remove 1 chip. Please enter 1.");
 					chipsRemoved = scanner.nextInt();
 				}
 				one.addToChips(1);
 				break;
 			}
 			System.out.println("There are " + numOfChips + " remaining.");
-			System.out.println(one.getName() + " has " + one.getChips() + " chips. \n" + two.getName() + " has " + two.getChips() + " chips");
+			System.out.println(one.getName() + " has " + one.getChips()
+					+ " chips. \n" + two.getName() + " has " + two.getChips()
+					+ " chips");
 			System.out.println("Your turn " + one.getName());
-			System.out.println(one.getName() + " please remove some chips. You're allowed to remove 1 to " + chipsAllowed);
+			System.out
+			.println(one.getName()
+					+ " please remove some chips. You're allowed to remove 1 to "
+					+ chipsAllowed);
 			chipsRemoved = scanner.nextInt();
 			while (chipsRemoved < 1 || chipsRemoved > chipsAllowed) {
-				System.out.println("Wrong number of chips removed. You're allowed to remove 1 to " + chipsAllowed);
+				System.out
+				.println("Wrong number of chips removed. You're allowed to remove 1 to "
+						+ chipsAllowed);
 				chipsRemoved = scanner.nextInt();
 			}
 			numOfChips -= chipsRemoved;
-			//			System.out.println("Number of chips remaining is " + numOfChips);
+			// System.out.println("Number of chips remaining is " + numOfChips);
 			one.addToChips(chipsRemoved);
-			System.out.println(one.getName() + " has " + one.getChips() + " chips. \n" + two.getName() + " has " + two.getChips() + " chips");
+			System.out.println(one.getName() + " has " + one.getChips()
+					+ " chips. \n" + two.getName() + " has " + two.getChips()
+					+ " chips");
 
 			if (numOfChips == 0) {
 				break;
 			}
 			if (numOfChips == 1) {
-				System.out.println(two.getName() + " There is 1 chip remaining. You can only take 1");
+				System.out.println(two.getName()
+						+ " There is 1 chip remaining. You can only take 1. Please enter 1.");
 				chipsRemoved = scanner.nextInt();
 				while (chipsRemoved != 1) {
 					System.out.println("You must remove 1 chip");
@@ -89,37 +110,46 @@ public class AssignmentOne {
 				two.addToChips(1);
 				break;
 			}
-			chipsAllowed = numOfChips % 2 == 0 ? numOfChips / 2: (numOfChips - 1) / 2;
+			chipsAllowed = numOfChips % 2 == 0 ? numOfChips / 2
+					: (numOfChips - 1) / 2;
 
 			System.out.println("There are " + numOfChips + " remaining.");
 			System.out.println("Your turn " + two.getName());
-			System.out.println(two.getName() + " please remove some chips. You're allowed to remove 1 to " + chipsAllowed);
+			System.out
+			.println(two.getName()
+					+ " please remove some chips. You're allowed to remove 1 to "
+					+ chipsAllowed);
 			chipsRemoved = scanner.nextInt();
 			while (chipsRemoved < 1 || chipsRemoved > chipsAllowed) {
-				System.out.println("Wrong number of chips removed. You're allowed to remove 1 to " + chipsAllowed);
+				System.out
+				.println("Wrong number of chips removed. You're allowed to remove 1 to "
+						+ chipsAllowed);
 				chipsRemoved = scanner.nextInt();
 			}
 			numOfChips -= chipsRemoved;
-			chipsAllowed = numOfChips % 2 == 0 ? numOfChips / 2: (numOfChips - 1) / 2;
+			chipsAllowed = numOfChips % 2 == 0 ? numOfChips / 2
+					: (numOfChips - 1) / 2;
 			two.addToChips(chipsRemoved);
 			System.out.println("Number of chips remaining is " + numOfChips);
-			System.out.println(one.getName() + " has " + one.getChips() + " chips. \n" + two.getName() + " has " + two.getChips() + " chips");
-			System.out.println("**************************************************************************************************************");
+			System.out.println(one.getName() + " has " + one.getChips()
+					+ " chips. \n" + two.getName() + " has " + two.getChips()
+					+ " chips");
+			System.out
+			.println("**************************************************************************************************************");
 
 		}
 
-		if(one.getChips() % 2 == 0) {
+		if (one.getChips() % 2 == 0) {
 			System.out.println(one.getName() + " wins.");
 			System.out.println(two.getName() + " You lose");
-		}
-		else {
+		} else {
 			System.out.println(two.getName() + " wins.");
 			System.out.println(one.getName() + "You lose :p");
 		}
 
 		System.out.println("Do you want to play again? Y/N");
 		if (scanner.next().equalsIgnoreCase("Y")) {
-			pileGame();
+			pileGame(scanner);
 		}
 
 		scanner.close();
@@ -132,13 +162,13 @@ public class AssignmentOne {
 	 * @param allowedGuesses
 	 *            number of chances to guess
 	 */
-	private static void numberGuessing(int randomNumber, int allowedGuesses) {
+	private static void numberGuessing(int randomNumber, int allowedGuesses, Scanner scanner) {
 		System.out.println("Please enter your guess.");
 		int counter = 0;
-		Scanner scanner = null;
+		// scanner = null;
 		try {
 
-			scanner = new Scanner(System.in);
+			//scanner = new Scanner(System.in);
 			int guess = 0;
 			do {
 				if (scanner.hasNextInt()) {
@@ -169,7 +199,8 @@ public class AssignmentOne {
 		}
 
 		finally {
-			scanner.close();
+			//			scanner.close();
+
 		}
 
 	}
@@ -184,7 +215,6 @@ class Player {
 		this.name = name;
 	}
 
-
 	public int getChips() {
 		return chips;
 	}
@@ -196,11 +226,14 @@ class Player {
 	public void addToChips(int chips) {
 		this.chips += chips;
 	}
+
 	public Player() {
 	}
 
 	public Player(String name) {
 		this.name = name;
 		this.chips = 0;
+
 	}
+
 }
